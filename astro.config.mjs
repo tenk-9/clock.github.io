44145cf3117af38dd8f8018dbba4cf42a636f25d
tenk-laptop <takuo6000@gmail.com>
2025-10-01 16:22:44 +0900

    fix base path for custom domain deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://tenk-9.github.io',
-  base: '/',
+  site: 'https://tenk.yamaguchi.jp',
+  base: '/clock.github.io',
 });
